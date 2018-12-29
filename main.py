9452cf9af1854682f53e051527e45c0327d415eb
Romchirick <comaro80@gmail.com>
2018-12-29 18:03:47 +0700

    хуй

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ size = width, height = 1280, 920
 speed = [1,1]
 black = 0, 0, 0
 screen = pygame.display.set_mode(size)
-
 image = pygame.image.load("ball.png")
 object = obj.Object(image, 30, 30, width, height)
+timer = pygame.time.Clock()
+
 
 def main():
     while True:
         update()
         render()
-
+        timer.tick(frame_rate)
 
 def render():
     screen.fill(black)
-
     object.render(screen)
     pygame.display.flip()
 
