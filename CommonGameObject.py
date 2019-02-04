86f48607e579ec61a445ff8585f3cc76a35fedbc
mnogoruk <lionless072@gmail.com>
2019-02-04 21:00:09 +0700

    added common game object class

---
+++ CommonGameObject.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+class CommonGameObject(pygame.sprite.Sprite):
+
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.x = x
+        self.y = y
+        self.ID = None
+
