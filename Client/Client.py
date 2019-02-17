30d3188cb44b60749bb254ceba6571f539e68c33
Romchirick <comaro80@gmail.com>
2019-02-17 15:06:47 +0700

    First working server
    Перва версия сервера которая работает!!!

---
+++ Client/Client.py
@@ -0,0 +1,44 @@
+import asyncore
+import threading
+import _pickle
+import socket
+
+
+class Client(asyncore.dispatcher, threading.Thread):
+    def __init__(self, host, port, name):
+        self.address = (host, port)
+        asyncore.dispatcher.__init__(self)
+        threading.Thread.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.name = name
+        self.connect(self.address)
+
+        self.input = []
+        self.data_to_write = []
+
+    def get_input(self):
+        inp = {self.name: self.input}
+        self.input = []
+        return inp
+
+    def add_output_data(self, data):
+        for obj in data:
+            self.data_to_write.append(obj)
+
+    def handle_connect(self):
+        print("Successfully connected to {}".format(self.address[0]))
+
+    def handle_error(self):
+        pass
+
+    def handle_write(self):
+        if bool(self.data_to_write):
+            self.send(_pickle.dumps(self.data_to_write.pop(0)))
+
+    def handle_read(self):
+        data = self.recv(1024)
+        if data:
+            self.input.append(_pickle.loads(data))
+
+    def run(self):
+        asyncore.loop()
---
+++ Server/Server.py
@@ -0,0 +1,41 @@
+import asyncore
+import socket
+import threading
+from Server.ClientHandler import ClientHandler
+
+MAX_CONNECTIONS = 4
+
+
+class Server(asyncore.dispatcher, threading.Thread):
+    def __init__(self, host, port):
+        self.address = (host, port)
+        asyncore.dispatcher.__init__(self)
+        threading.Thread.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind(self.address)
+        self.listen(MAX_CONNECTIONS)
+
+        self.data_to_write = []
+        self.connections = []
+
+    def get_input(self):
+        inp = dict()
+        for i in range(len(self.connections)):
+            inp[i] = self.connections[i].input
+            self.connections[i].input = []
+        return inp
+
+    def handle_accept(self):
+        info = self.accept()
+        if info is not None:
+            print("New connection form: {}".format(info))
+            self.connections.append(ClientHandler(info[0], len(self.connections)))
+
+    def add_output_data(self, data):
+        for obj in data:
+            for client in self.connections:
+                client.data_to_write.append(obj)
+
+    def run(self):
+        asyncore.loop()
