4071eb098413ce0752c297eb6c079c685d83329d
Romchirick <comaro80@gmail.com>
2019-03-15 10:47:10 +0700

    Почистил от ненужных файлов

--- TCPConnection/ServerInterface.py
+++
@@ -1,6 +0,0 @@
-class ServerConnectionInterface:
-    def __init__(self):
-        pass
-
-    def func(self):
-        pass
--- TCPConnection/TestDataClass.py
+++
@@ -1,3 +0,0 @@
-class DataClass:
-    def __init__(self):
-        data = [1 for _ in range(1000)]
